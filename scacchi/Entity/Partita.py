be2f10dbe5c8a4326ef654dacd7173b76a95bf9a
FabioPSTELL <fabiopistillo4331@gmail.com>
2025-05-11 15:43:11 +0200

    Update Partita.py

---
+++ scacchi/Entity/Partita.py
@@ -0,0 +1,19 @@
+class Partita:
+    """Classe di tipo <<  Entity >>, per la gestione della partita."""
+    
+    def __init__(self):
+        self.__stato_partita = 0  # 0 = in corso, 1 = non in corso
+        self.__turno = 0  # 0= turno del bianco, 1 = turno del nero
+
+    def get_turno(self):
+        return self.__turno
+    
+    def cambiaturno(self):                  # passa il turno
+        self.__turno = 1 - self.__turno 
+
+    def get_stato_partita(self):
+        return self.__stato_partita
+    
+
+    def cambia_stato_partita(self):                             # cambia lo stato della partita
+            self.__stato_partita =1-self.__stato_partita
